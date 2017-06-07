369e378c6585ad5309e43736dd071d9d27ea1cb3
Andrew Lim <andyl319@yahoo.com>
2017-06-07 13:49:21 -0700

    add sessions controller

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeaderContainer from './header/header_container';
-import TrackDisplayContainer from './track/track_display_container';
+
 
 const App = ({ children }) => (
   <div>
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,12 +2,6 @@ import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import App from './app';
 import React from 'react';
 import {Provider} from 'react-redux';
-import {requestAllTracks, requestTrack} from '../actions/track_actions';
-import SessionContainer from './session/session_container';
-import TrackDisplayContainer from './track/track_display_container';
-import TrackIndexContainer from './track/track_index_container';
-import TrackShowContainer from './track/track_show_container';
-import TrackCommentContainer from './track_comments/track_comments_container';
 
 const Root = ({store}) => {
 
@@ -23,11 +17,6 @@ const Root = ({store}) => {
     <Provider store={ store }>
       <Router onUpdate={() => window.scrollTo(0, 0)} history={ hashHistory }>
         <Route path="/" component={ App } >
-          <IndexRoute component={ TrackDisplayContainer } />
-          <Route path="all" component={ TrackIndexContainer } />
-          <Route path="tracks/:id" component={ TrackShowContainer } >
-            <IndexRoute component={ TrackCommentContainer } />
-          </Route>
           <Route path="login" component={ SessionContainer } onEnter={_redirectIfLoggedIn}/>
           <Route path="signup" component={ SessionContainer } onEnter={_redirectIfLoggedIn}/>
         </Route>
